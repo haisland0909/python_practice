1fe94f2de9ffafa709cba5675edbc676e5be54c7
haisland0909 <awaawashima@gmail.com>
2015-04-09 10:50:04 +0900

    pil_sample to class

--- 実践コンピュータビジョン/pilsample/testpilsample.py
+++ 実践コンピュータビジョン/pilsample/testpilsample.py
@@ -1,8 +1,9 @@
 import unittest, pilsample
 
 class Test_testpilsample(unittest.TestCase):
-    def test_get_sample_image(self):
-        im = pilsample.get_sample_image()
+    def test_get_image(self):
+        obj = pilsample.PilSample()
+        im  = obj.get_image()
         self.assertEqual("JPEG", im.format)
         self.assertEqual((1280, 960), im.size)
         self.assertEqual("RGB", im.mode)
