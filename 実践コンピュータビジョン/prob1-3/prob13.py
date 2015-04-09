2a1fb0e2b70841a726eddeca2e454b87ec2197ca
haisland0909 <awaawashima@gmail.com>
2015-04-10 08:56:28 +0900

    1-3 yet

---
+++ 実践コンピュータビジョン/prob1-3/prob13.py
@@ -0,0 +1,21 @@
+﻿# -*- coding: utf-8 -*-
+import sys, numpy
+sys.path.append('../prob1-1')
+# ここまでおまじない
+from prob11 import Prob11
+
+class Prob13(Prob11):
+    # Self-Quotient画像を作成する
+    def make_self_quotient(self, sd):
+        old_im            = self._array_image.copy()
+        self._add_gaussian_filter(sd)
+        new_im            = self._array_image.copy()
+        if self._is_color:
+            for i in range(3):
+                new_im[:, :, i] = old_im[:, :, i] / new_im[:, :, i]
+                new_im[:, :, i] = 255.0 * new_im[:, :, i] / numpy.amax(new_im[:, :, i])
+        else:
+            new_im = old_im / new_im
+            new_im = 255.0 * new_im / numpy.amax(new_im)
+        self._array_image = new_im
+        self._convert_image()
---
+++ 実践コンピュータビジョン/prob1-3/testprob13.py
@@ -0,0 +1,19 @@
+import unittest, prob13
+class Test_testprob13(unittest.TestCase):
+    def test_make_self_quotient(self):
+        obj   = prob13.Prob13()
+        sd_1  = obj.make_self_quotient(1)
+        obj.save('sd_1.jpg')
+        obj   = prob13.Prob13()
+        sd_1  = obj.make_self_quotient(5)
+        obj.save('sd_5.jpg')
+        obj   = prob13.Prob13()
+        sd_1  = obj.make_self_quotient(10)
+        obj.save('sd_10.jpg')
+        obj   = prob13.Prob13()
+        sd_1  = obj.make_self_quotient(20)
+        obj.save('sd_20.jpg')
+        
+
+if __name__ == '__main__':
+    unittest.main()
