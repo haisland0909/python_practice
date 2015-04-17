2a32ab17cf58c2d165b9aaab357c5bbb270053ab
haisland0909 <awaawashima@gmail.com>
2015-04-17 10:33:28 +0900

    svm

--- scikit-learn-tutorial/supervisedlearningsample/supervisedlearningsample.py
+++ scikit-learn-tutorial/supervisedlearningsample/supervisedlearningsample.py
@@ -1,7 +1,7 @@
 ﻿# -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import datasets, linear_model
+from sklearn import datasets, linear_model, svm
 from matplotlib.colors import ListedColormap
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 from sklearn.decomposition import PCA
@@ -75,6 +75,7 @@ class LinearSample(object):
         self._shrink_x = np.c_[ .5, 1].T
         self._shrink_y = [.5, 1]
         self._shrink_t = np.c_[ 0, 2].T
+        self._alphas   = np.logspace(-4, -1, 6)
 
     def get_diabetes_x(self):
 
@@ -83,10 +84,11 @@ class LinearSample(object):
     def get_diabetes_y(self):
 
         return self._diabetes.target
+    
 
     def do_linear_regression(self):
-        diabetes_X      = self.get_diabetes_x()[:, np.newaxis]
-        diabetes_X_temp = diabetes_X[:, :, 2]
+        diabetes_X       = self.get_diabetes_x()[:, np.newaxis]
+        diabetes_X_temp  = diabetes_X[:, :, 2]
         diabetes_X_train = diabetes_X_temp[:-20]
         diabetes_y_train = self.get_diabetes_y()[:-20]
         diabetes_X_test  = diabetes_X_temp[-20:]
@@ -103,6 +105,47 @@ class LinearSample(object):
 
         plt.savefig("linear_regression.png")
 
+    def plot_sparce_data(self):
+        diabetes = self.get_diabetes_x()
+        indices  = (0, 1)
+        X_train  = self.get_diabetes_x()[:-20, indices]
+        X_test   = self.get_diabetes_x()[-20:, indices]
+        y_train  = self.get_diabetes_y()[:-20]
+        y_test   = self.get_diabetes_y()[-20:]
+        ols = linear_model.LinearRegression()
+        ols.fit(X_train, y_train)
+        elev = 43.5
+        azim = -110
+        self._plot_figs(1, elev, azim, X_train, y_train, ols)
+        
+        elev = -.5
+        azim = 0
+        self._plot_figs(2, elev, azim, X_train, y_train, ols)
+        
+        elev = -.5
+        azim = 90
+        self._plot_figs(3, elev, azim, X_train, y_train, ols)
+
+    def _plot_figs(self, fig_num, elev, azim, X_train, y_train, clf):
+        fig = plt.figure(fig_num, figsize=(4, 3))
+        plt.clf()
+        ax = Axes3D(fig, elev=elev, azim=azim)
+
+        ax.scatter(X_train[:, 0], X_train[:, 1], y_train, c='k', marker='+')
+        ax.plot_surface(np.array([[-.1, -.1], [.15, .15]]),
+                        np.array([[-.1, .15], [-.1, .15]]),
+                        clf.predict(np.array([[-.1, -.1, .15, .15],
+                                              [-.1, .15, -.1, .15]]).T
+                                    ).reshape((2, 2)),
+                        alpha=.5)
+        ax.set_xlabel('X_1')
+        ax.set_ylabel('X_2')
+        ax.set_zlabel('Y')
+        ax.w_xaxis.set_ticklabels([])
+        ax.w_yaxis.set_ticklabels([])
+        ax.w_zaxis.set_ticklabels([])
+        plt.savefig("sparce_data_%d.jpg" % fig_num)
+
     def do_linear_few_data(self, fit_way = linear_model.LinearRegression()):
         plt.figure() 
         np.random.seed(0)
@@ -114,6 +157,116 @@ class LinearSample(object):
         plt.title("Faw Data Linear Regression BY %s" % fit_way.__class__.__name__)
         plt.savefig("linear_few_%s.jpg" % fit_way.__class__.__name__)
 
+    def do_lasso(self):
+        diabetes_X       = self.get_diabetes_x()
+        diabetes_X_train = diabetes_X[:-20]
+        diabetes_y_train = self.get_diabetes_y()[:-20]
+        diabetes_X_test  = diabetes_X[-20:]
+        diabetes_y_test  = self.get_diabetes_y()[-20:]
+        regr             = linear_model.Lasso()
+        scores           = [regr.set_params(alpha=alpha).fit(diabetes_X_train, diabetes_y_train).score(diabetes_X_test, diabetes_y_test) for alpha in self._alphas]
+        best_alpha       = self._alphas[scores.index(max(scores))]
+        regr.alpha       = best_alpha
+        regr.fit(diabetes_X_train, diabetes_y_train)
+        print(regr.coef_)
+
+    def model(self, x):
+
+        return 1 / (1 + np.exp(-x))
+
+    def plot_logistics(self):
+        xmin, xmax = -5, 5
+        n_samples  = 100
+        np.random.seed(0)
+        X          = np.random.normal(size=n_samples)
+        y          = (X > 0).astype(np.float)
+        X[X > 0]   *= 4
+        X          += .3 * np.random.normal(size=n_samples)       
+        X          = X[:, np.newaxis]
+        # run the classifier
+        clf        = linear_model.LogisticRegression(C=1e5)
+        clf.fit(X, y)
+        
+        # and plot the result
+        plt.figure(1, figsize=(4, 3))
+        plt.clf()
+        plt.scatter(X.ravel(), y, color='black', zorder=20)
+        X_test = np.linspace(-5, 10, 300)
+        loss   = self.model(X_test * clf.coef_ + clf.intercept_).ravel()
+        plt.plot(X_test, loss, color='blue', linewidth=3)
+        
+        ols    = linear_model.LinearRegression()
+        ols.fit(X, y)
+        plt.plot(X_test, ols.coef_ * X_test + ols.intercept_, linewidth=1)
+        plt.axhline(.5, color='.5')
+        plt.ylabel('y')
+        plt.xlabel('X')
+        plt.xticks(())
+        plt.yticks(())
+        plt.ylim(-.25, 1.25)
+        plt.xlim(-4, 10)
+        plt.savefig("logistics_regression.jpg")
+
+class SVMSample(object):
+    def __init__(self):
+        self._iris = datasets.load_iris()
+
+    def get_iris_x(self):
+
+        return self._iris.data
+
+    def get_iris_y(self):
+
+        return self._iris.target
+
+    def plot_svm_margin(self):
+        np.random.seed(0)
+        X = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
+        Y = [0] * 20 + [1] * 20
+        
+        # figure number
+        fignum = 1
+        
+        # fit the model
+        for name, penalty in (('linear', 1), ('poly', 0.05), ('rbf', 1)):
+        
+            clf = svm.SVC(kernel=name, C=penalty)
+            clf.fit(X, Y)
+        
+            # plot the line, the points, and the nearest vectors to the plane
+            plt.figure(fignum, figsize=(4, 3))
+            plt.clf()
+            # サポートベクターには大きめの丸を重ねて描く
+            plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=80,
+                        facecolors='none', zorder=10)
+            plt.scatter(X[:, 0], X[:, 1], c=Y, zorder=10, cmap=plt.cm.Paired)
+        
+            plt.axis('tight')
+            x_min = -4.8
+            x_max = 4.2
+            y_min = -6
+            y_max = 6
+        
+            XX, YY = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
+            Z = clf.decision_function(np.c_[XX.ravel(), YY.ravel()])
+        
+            # Put the result into a color plot
+            Z = Z.reshape(XX.shape)
+            plt.figure(fignum, figsize=(4, 3))
+            plt.pcolormesh(XX, YY, Z > 0, cmap=plt.cm.Paired)
+            plt.contour(XX, YY, Z, colors=['k', 'k', 'k'], linestyles=['--', '-', '--'],
+                levels=[-.5, 0, .5])
+        
+            plt.xlim(x_min, x_max)
+            plt.ylim(y_min, y_max)
+        
+            plt.xticks(())
+            plt.yticks(())
+            plt.savefig("svm_sample_%s.jpg" % name)
+            fignum = fignum + 1
+
+
+        
 if __name__ == '__main__':
     obj = KNearestSample()
     obj.plot_iris_data()
--- scikit-learn-tutorial/supervisedlearningsample/testsupervisedlearningsample.py
+++ scikit-learn-tutorial/supervisedlearningsample/testsupervisedlearningsample.py
@@ -21,6 +21,21 @@ class Test_testsupervisedlearningsample(unittest.TestCase):
         obj.do_linear_few_data()
         obj.do_linear_few_data(linear_model.Ridge(alpha=.1))
 
+    def test_plot_sparce_data(self):
+        obj = supervisedlearningsample.LinearSample()
+        obj.plot_sparce_data()
+
+    def test_do_lasso(self):
+        obj = supervisedlearningsample.LinearSample()
+        obj.do_lasso()
+
+    def test_plot_logistics(self):
+        obj = supervisedlearningsample.LinearSample()
+        obj.plot_logistics()
+
+    def test_plot_svm_margin(self):
+        obj = supervisedlearningsample.SVMSample()
+        obj.plot_svm_margin()
 
 if __name__ == '__main__':
     unittest.main()
