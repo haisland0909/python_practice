3be9919f0f9516ba8cf6a925695ad1e602bff27c
haisland0909 <awaawashima@gmail.com>
2015-04-17 14:33:51 +0900

    k-means-lena

--- scikit-learn-tutorial/unsupervisedlearningsample/testunsupervisedlearningsample.py
+++ scikit-learn-tutorial/unsupervisedlearningsample/testunsupervisedlearningsample.py
@@ -5,5 +5,9 @@ class Test_testunsupervisedlearningsample(unittest.TestCase):
         obj = unsupervisedlearningsample.KMeansSample()
         obj.plot_iris_k_mean()
 
+    def test_plot_lena_k_mean(self):
+        obj = unsupervisedlearningsample.KMeansSample()
+        obj.plot_lena_k_mean()
+
 if __name__ == '__main__':
     unittest.main()
--- scikit-learn-tutorial/unsupervisedlearningsample/unsupervisedlearningsample.py
+++ scikit-learn-tutorial/unsupervisedlearningsample/unsupervisedlearningsample.py
@@ -58,7 +58,7 @@ class KMeansSample(object):
                       X[y == label, 0].mean() + 1.5,
                       X[y == label, 2].mean(), name,
                       horizontalalignment='center',
-                      bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+                      bbox=dict(alpha = .5, edgecolor = 'w', facecolor = 'w'))
         # Reorder the labels to have colors matching the cluster results
         y = np.choose(y, [1, 2, 0]).astype(np.float)
         ax.scatter(X[:, 3], X[:, 0], X[:, 2], c = y)
@@ -70,4 +70,57 @@ class KMeansSample(object):
         ax.set_ylabel('Sepal length')
         ax.set_zlabel('Petal length')
         plt.savefig("k-means-iris-truth.jpg")
- 
+
+    def plot_lena_k_mean(self):
+        n_clusters = 5
+        np.random.seed(0)
+        lena       = self._lena
+        X          = lena.reshape((-1, 1))  # We need an (n_sample, n_feature) array
+        k_means    = cluster.KMeans(n_clusters=n_clusters, n_init=4)
+        k_means.fit(X)
+        values     = k_means.cluster_centers_.squeeze()
+        labels     = k_means.labels_
+        
+        # create an array from labels and values
+        lena_compressed       = np.choose(labels, values)
+        lena_compressed.shape = lena.shape
+        
+        vmin       = lena.min()
+        vmax       = lena.max()
+        
+        # original lena
+        plt.figure(1, figsize=(3, 2.2))
+        plt.imshow(lena, cmap=plt.cm.gray, vmin=vmin, vmax=256)
+        plt.savefig("lena_original.jpg")
+        
+        # compressed lena
+        plt.figure(2, figsize=(3, 2.2))
+        plt.imshow(lena_compressed, cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
+        plt.savefig("lena_k_means.jpg")
+        # equal bins lena
+        regular_values = np.linspace(0, 256, n_clusters + 1)
+        
+        regular_labels = np.searchsorted(regular_values, lena) - 1
+        regular_values = .5 * (regular_values[1:] + regular_values[:-1])  # mean
+        regular_lena = np.choose(regular_labels.ravel(), regular_values)
+        regular_lena.shape = lena.shape
+        
+        plt.figure(3, figsize=(3, 2.2))
+        plt.imshow(regular_lena, cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
+        plt.savefig("lena_regular.jpg")
+        
+        # histogram
+        plt.figure(4, figsize=(3, 2.2))
+        plt.clf()
+        plt.axes([.01, .01, .98, .98])
+        plt.hist(X, bins=256, color='.5', edgecolor='.5')
+        plt.yticks(())
+        plt.xticks(regular_values)
+        values = np.sort(values)
+        for center_1, center_2 in zip(values[:-1], values[1:]):
+            plt.axvline(.5 * (center_1 + center_2), color='b')
+        
+        for center_1, center_2 in zip(regular_values[:-1], regular_values[1:]):
+            plt.axvline(.5 * (center_1 + center_2), color='b', linestyle='--')
+
+        plt.savefig("lena_hist.jpg")
